a5bd1b4f15cb95663e71796d8f4564409d5da4bb
Người Trẻ <110954389+NamSellsFish@users.noreply.github.com>
2022-12-16 10:07:42 +0700

    test

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from flask import Flask, render_template
 
 # Get Data
 ses = requests.session()
-ses = requests.get(f"https://github.com/awesome-jobs/vietnam/issues?page=2&q=is%3Aissue+is%3Aopen")
+ses = requests.get(
+    f"https://github.com/awesome-jobs/vietnam/issues?page=2&q=is%3Aissue+is%3Aopen")
 soup = BeautifulSoup(ses.content, "html.parser")
 titles = [title.text for title in
           soup.find_all("a", class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title")]
 details = [f"https://github.com{detail.attrs.get('href')}" for detail in
            soup.find_all("a", class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title")]
-openedDates = [date.text for date in soup.find_all("relative-time", class_="no-wrap")]
+openedDates = [date.text for date in soup.find_all(
+    "relative-time", class_="no-wrap")]
 jobs = []
 for i in range(len(titles)):
     jobs.append((titles[i], openedDates[i], details[i]))
@@ -20,14 +22,13 @@ for i in range(len(titles)):
 con = sqlite3.connect("awesomeJobs.db", check_same_thread=False)
 cur = con.cursor()
 res = cur.execute("SELECT name FROM sqlite_master  WHERE name='jobs'")
-if res.fetchone() is None:
-    cur.execute("CREATE TABLE jobs(titles, opened, details)")
-    for job in jobs:
-        cur.execute(f"""
+cur.execute("CREATE TABLE IF NOT EXISTS jobs(titles, opened, details)")
+for job in jobs:
+    cur.execute(f"""
         INSERT INTO jobs   VALUES
                 {job}
         """)
-        con.commit()
+    con.commit()
 
 # Work with website
 app = Flask("myapp")
@@ -37,7 +38,8 @@ app = Flask("myapp")
 def showTheJobs():
     jobsJSON = []
     for top, row in enumerate(cur.execute("SELECT titles,opened,details FROM jobs ORDER BY titles"), 1):
-        jobsJSON.append({"No": top, "title": row[0], "opened": row[1], "detail": row[2]})
+        jobsJSON.append(
+            {"No": top, "title": row[0], "opened": row[1], "detail": row[2]})
 
     return render_template("index.html", jobs=jobsJSON)
 
